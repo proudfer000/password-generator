1f70e286faf673c14c9c72075ba5fda7fdb4541d
Fernando Hernandez <ferehernandez000@gmail.com>
2020-10-17 01:15:18 -0400

    adding funtions

--- script.js
+++ script.js
@@ -2,30 +2,42 @@ var lowerCaseChr = "qwertyuiopasdfghjklzxcvbnm";
 var upperCaseChr = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var numericChr = "0123456789";
 var specialChr = "~!@#$%&=+<>?";
+var allChrSum =""
 
+var checkboxLowerCase = document.querySelector("#lowerCase");
+var checkboxUpperCase = document.querySelector("#upperCase");
+var checkboxNumeric = document.querySelector("#numeric");
+var checkboxSpecialChr = document.querySelector("#specialCharacters");
+
+
+
+function test() {
+  if(document.getElementById("lowerCase").checked==true){
+    alert ("mierda");
+    allChrSum = allChrSum + numericChr;
+    allChrSum = allChrSum + specialChr;
+
+    
+  }
+  
+  
+}
+
+function clearValue() {
+  allChrSum = ""
+  
+}
 
 var passLength = document.querySelector("#passLength");
 var copy = document.querySelector("#copyBtn");
-var checkboxlowerCase
-var checkboxlowerCase
-var checkboxlowerCase
-var checkboxlowerCase
+var generatePass = document.querySelector("#generateBtn");
+var clear = document.querySelectorAll("input[type=checkbox]");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(checkboxLowerCase.checked);
+console.log(checkboxUpperCase.checked);
+console.log(checkboxNumeric.checked);
+console.log(checkboxSpecialChr.checked);
+//console.log(test);
 
 
 
@@ -42,3 +54,6 @@ function copyText(event) {
     document.execCommand("copy");
   }
   copy.addEventListener("click", copyText);
+  generatePass.addEventListener("click", test);
+  clear.addEventListener("click", )
+  
