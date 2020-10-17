142365ee67c577c140b42735ca988c20d21a59ca
Fernando Hernandez <ferehernandez000@gmail.com>
2020-10-17 03:22:09 -0400

    added new statements and var

--- script.js
+++ script.js
@@ -2,36 +2,55 @@ var lowerCaseChr = "qwertyuiopasdfghjklzxcvbnm";
 var upperCaseChr = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var numericChr = "0123456789";
 var specialChr = "~!@#$%&=+<>?";
-var allChrSum =""
+var allChrSum ="";
+
 
 var checkboxLowerCase = document.querySelector("#lowerCase");
 var checkboxUpperCase = document.querySelector("#upperCase");
 var checkboxNumeric = document.querySelector("#numeric");
 var checkboxSpecialChr = document.querySelector("#specialCharacters");
+var passLength = document.querySelector("#passLength");
+var copy = document.querySelector("#copyBtn");
+var generatePass = document.querySelector("#generateBtn");
 
 
 
-function test() {
-  if(document.getElementById("lowerCase").checked==true){
-    alert ("mierda");
-    allChrSum = allChrSum + numericChr;
-    allChrSum = allChrSum + specialChr;
+function test(event) {
+  
+  if(document.getElementById("lowerCase").checked==true 
+  || document.getElementById("upperCase").checked==true
+  || document.getElementById("numeric").checked==true
+  || document.getElementById("specialCharacters").checked==true){
+    var allChrSumArray ="";
+      if(document.getElementById("lowerCase").checked==true){
+        allChrSum = allChrSum + lowerCaseChr;
+   }
+      if(document.getElementById("upperCase").checked==true){
+        allChrSum = allChrSum + upperCaseChr;
+      }
+      if(document.getElementById("numeric").checked==true){
+        allChrSum = allChrSum + numericChr;
+      }
+      if(document.getElementById("specialCharacters").checked==true){
+        allChrSum = allChrSum + specialChr;
+      }
+      allChrSum = allChrSum.split("");
+  }
 
-    
+  else {
+    alert ("Please check at least one option")
   }
   
   
 }
 
 function clearValue() {
-  allChrSum = ""
+  allChrSum = "";
   
 }
 
-var passLength = document.querySelector("#passLength");
-var copy = document.querySelector("#copyBtn");
-var generatePass = document.querySelector("#generateBtn");
-var clear = document.querySelectorAll("input[type=checkbox]");
+
+
 
 console.log(checkboxLowerCase.checked);
 console.log(checkboxUpperCase.checked);
@@ -53,7 +72,12 @@ function copyText(event) {
     copyText.setSelectionRange(0, 99999); /*For mobile devices*/
     document.execCommand("copy");
   }
+
+  checkboxLowerCase.addEventListener("click", clearValue);
+  checkboxUpperCase.addEventListener("click", clearValue);
+  checkboxNumeric.addEventListener("click", clearValue);
+  checkboxSpecialChr.addEventListener("click", clearValue);
   copy.addEventListener("click", copyText);
   generatePass.addEventListener("click", test);
-  clear.addEventListener("click", )
+  
   
